5609e641b700b7a1009ae81ec36255727067599c
ujjawalgupta23 <ujjawalgupta23@gmail.com>
2023-04-29 14:42:16 +0530

    added contest learning

---
+++ data-structures-operators/contest1.js
@@ -0,0 +1,86 @@
+'use strict';
+
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+        [
+            'Neuer',
+            'Pavard',
+            'Martinez',
+            'Alaba',
+            'Davies',
+            'Kimmich',
+            'Goretzka',
+            'Coman',
+            'Muller',
+            'Gnarby',
+            'Lewandowski',
+        ],
+        [
+            'Burki',
+            'Schulz',
+            'Hummels',
+            'Akanji',
+            'Hakimi',
+            'Weigl',
+            'Witsel',
+            'Hazard',
+            'Brandt',
+            'Sancho',
+            'Gotze',
+        ],
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+        team1: 1.33,
+        x: 3.25,
+        team2: 6.5,
+    },
+};
+
+// 1.
+const players1 = [...game.players[0]];
+console.log(players1);
+
+const players2 = [...game.players[1]];
+console.log(players2);
+
+// 2.
+const [gk, ...feildPlayers] = players1;
+console.log(gk, feildPlayers);
+
+// 3.
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+// 4.
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+// 5.
+// const { team1, x: draw, team2 } = game.odds;
+
+// or
+const {
+    odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+// 6.
+const printGoals = function (...names) {
+    console.log(`Goals scored are ${names.length}`);
+};
+
+printGoals('Ujjawal', 'Naman');
+printGoals(...players1);
+printGoals(...game.scored);
+
+// 7.
+console.log();
+
+// 8.
+team1 > team2 && console.log('Team1 is more likely to win');
+team2 > team1 && console.log('Team2 is more likey to win');
