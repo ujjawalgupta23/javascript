ca21e179bb9ab6f29b382b1eef667597858efba9
ujjawalgupta23 <ujjawalgupta23@gmail.com>
2023-04-30 14:16:45 +0530

    challenge-2

---
+++ data-structures-operators/11.challenge-2.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+        [
+            'Neuer',
+            'Pavard',
+            'Martinez',
+            'Alaba',
+            'Davies',
+            'Kimmich',
+            'Goretzka',
+            'Coman',
+            'Muller',
+            'Gnarby',
+            'Lewandowski',
+        ],
+        [
+            'Burki',
+            'Schulz',
+            'Hummels',
+            'Akanji',
+            'Hakimi',
+            'Weigl',
+            'Witsel',
+            'Hazard',
+            'Brandt',
+            'Sancho',
+            'Gotze',
+        ],
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+        team1: 1.33,
+        x: 3.25,
+        team2: 6.5,
+    },
+};
+
+// 1.
+for (const [index, player] of game.scored.entries()) {
+    console.log(`Goal ${index + 1}: ${player}`);
+}
+
+// 2.
+const odds = Object.values(game.odds);
+console.log(odds);
+
+let sum = 0;
+for (const val of Object.values(game.odds)) {
+    sum += val;
+}
+const oddLength = Object.values(game.odds).length;
+const average = sum / oddLength;
+console.log(average);
+
+// 3.
+for (const [team, odd] of Object.entries(game.odds)) {
+    const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+    console.log(`Odd of ${teamStr} ${odd}`);
+}
