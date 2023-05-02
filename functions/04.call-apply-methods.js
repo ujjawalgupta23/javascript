59a8114e55888976d755b6cc3af9cf634a710c7b
ujjawalgupta23 <ujjawalgupta23@gmail.com>
2023-05-02 12:04:04 +0530

    add bind()

--- functions/04.call-apply-methods.js
+++ functions/04.call-apply-methods.js
@@ -28,6 +28,7 @@ const book = lufthansa.book;
 // book(23, 'Naman Gupta'); // this will not work, because this will point to undefined
 
 // we have to use call method to use object function outside
+// call manually sets this keyword to any function call
 book.call(lufthansa, 220, 'Yojan Saini'); //here this will point to lufthansa
 console.log(lufthansa);
 
@@ -48,3 +49,50 @@ book.call(swiss, ...['670', 'Nitin Mishra']);
 const flightData = [890, 'Rishabh Sehgal'];
 book.apply(swiss, flightData);
 book.call(eurowings, ...flightData);
+
+// Bind method
+// the bind() method returns the function binding this -> to the object passed to it
+
+// this will not call the book function, instead it will return new function where this keyword will be set to eurowings
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(978, 'Utkarsh Vasudeva');
+
+// Partial Applications - passing some parameters
+const bookEW23 = book.bind(swiss, 23);
+bookEW23('Ujjawal Gupta', 62); // any extra passed argument will be ignored like 62 here
+
+// With Event Listners
+// In Event Handler function, this always points to the element on which it is attached to
+lufthansa.planes = 200;
+lufthansa.buyplane = function () {
+    console.log(this);
+    this.planes++;
+    console.log(this.planes);
+};
+
+// here this will point to button element
+document
+    .querySelector('.buy')
+    .addEventListener('click', lufthansa.buyplane.bind(lufthansa));
+
+// Partial Application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * .23
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+// HOF
+const addT = function (rate, value) {
+    return function (value) {
+        return value + value * rate
+    }
+}
+
+console.log(addT(.23)(100));
